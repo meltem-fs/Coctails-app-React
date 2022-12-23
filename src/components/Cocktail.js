cc3e60a75354488deb37621b3ddf7e03b4f2df2c
MELTEM <meltemfsanaldi@gmail.com>
2022-12-23 12:26:07 +0300

    card

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,12 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Cocktail = () => {
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions, Grid } from "@mui/material";
+
+const Cocktail = ({ coctail }) => {
+  // console.log(coctail);
+
   return (
     <div>
-      <h2>cocktail component</h2>
+      {coctail &&
+        coctail.map((item) => {
+          return (
+            <Grid sx={{
+              display:"flex",
+            }} >
+              <Card sx={{ maxWidth: 400 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={item.strDrinkThumb}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {item.strDrink}
+                    </Typography>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {item.strGlass}
+                    </Typography>
+                    <Typography gutterBottom variant="p" component="div">
+                      {item.strAlcoholic}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Details
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
     </div>
-  )
-}
+  );
+};
 
-export default Cocktail
+export default Cocktail;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,19 +1,50 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import axios from "axios";
+import Cocktail from "./Cocktail";
+import { Grid } from "@mui/material";
 
 const SearchForm = () => {
+
+  const [search, setSearch] = useState("")
+  const [coctail, setCoctail] = useState("")
+  console.log(search);
+
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`;
+
+  const getApi = async()=>{
+    const {data} = await axios(url)
+    console.log(data.drinks)
+    setCoctail(data.drinks)
+  }
+
+  useEffect(() => {
+   getApi()
+  }, [search])
+  
+
  return (
-   <Box
-     sx={{
-       width: 500,
-       maxWidth: "100%",
-       padding:"2rem",
-       margin:"0 auto"
-     }}
-   >
-     <TextField fullWidth label="search coctail" id="fullWidth" color="error"/>
-   </Box>
+   <Grid>
+     <Box
+       sx={{
+         width: 500,
+         maxWidth: "100%",
+         padding: "2rem",
+         margin: "0 auto",
+       }}
+     >
+       <TextField
+         fullWidth
+         label="search coctail"
+         color="error"
+         onChange={(e) => setSearch(e.target.value)}
+         value={search}
+         id="search"
+       />
+     </Box>
+     <Cocktail coctail={coctail}/>
+   </Grid>
  );
 };
 
